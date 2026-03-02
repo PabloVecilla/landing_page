34000b4c2e28d3953127b2dfd464f0385a957474
PabloVecilla <pablovecilla8@gmail.com>
2026-03-02 13:13:54 +0100

    añadida lista de la compra a index.html

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Hello, world!"); 
+
+/*
+git init
+git status
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote -v   --> muestra el repositorio al que se va a subir el commit
+git remote add origin https://github.com/PabloVecilla/landing_page.git
+git push -u origin main */
